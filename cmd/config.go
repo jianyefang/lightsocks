0e87369f0b9dc34a2d2c5551013b9e56010b8808
jianyefang <76590965+jianyefang@users.noreply.github.com>
2022-10-11 13:30:25 +0800

    Update config.go

--- cmd/config.go
+++ cmd/config.go
@@ -22,14 +22,8 @@ type Config struct {
 }
 
 func init() {
-	home, _ := "/"
-	// 默认的配置文件名称
-	configFilename := "config.json"
-	// 如果用户有传配置文件，就使用用户传入的配置文件
-	if len(os.Args) == 2 {
-		configFilename = os.Args[1]
-	}
-	configPath = path.Join(home, configFilename)
+	
+	configPath = "/config.json"
 }
 
 // 保存配置到配置文件
